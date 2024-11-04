988316df05d2c43c03eb077824a0c93be4183e52
brunoressiguierbarbosa <bruno.ressiguier.barbosa@escola.pr.gov.br>
2024-11-04 12:26:35 -0300

    carga_adicionar-dados

---
+++ js-aparecida-nutri-2-tds-main/js/principal.js
@@ -0,0 +1,75 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var trPacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < trPacientes.length; i++) {
+
+    var trPaciente = trPacientes[i];
+
+    var tdPeso = trPaciente.querySelector(".info-peso");
+    var tdAltura = trPaciente.querySelector(".info-altura");
+    var tdImc = trPaciente.querySelector(".info-imc");
+
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        pesoEhValido = false;
+        trPaciente.classList.add("paciente-invalido");
+        tdPeso.classList.add("peso-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        alturaEhValida = false
+        trPaciente.classList.add("paciente-invalido");
+        tdAltura.classList.add("altura-invalida");
+  
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = (peso / (altura * altura));
+        tdImc.textContent = imc.toFixed(2);
+    } else {
+        tdImc.textContent = "Altura e/ou peso inválidos!"
+    }
+}
+
+var botaoAdicionar = document.querySelector("adicionar-paciente")
+botaoAdicionar .addEventListener("click", function (event) {
+    event.preventDefault();
+    //console.log("01, cliquei no botao");
+
+    var form = document.querySelector("#form-adicionar");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.gordura.vale;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("Tr");
+
+    var nomeTd = document.createElement("Td");
+    var pesoTd = document.createElement("Td");
+    var alturaTd = document.createElement("Td");
+    var gorduraTd = document.createElement("Td");
+    var imcTd = document.createElement("Td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela. appendChild(pacienteTr)
+
+} );
